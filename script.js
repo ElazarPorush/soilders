118b79954b32a74b6ca11d9685b6f47364fb5f1f
Elazar Porush <lzrprws@gmail.com>
2024-09-12 15:57:22 +0300

    almost finish

---
+++ script.js
@@ -0,0 +1,124 @@
+const GetAllSoldiers = () => {
+    const soldiers = JSON.parse(localStorage.getItem("soldiers"))
+    return soldiers
+}
+
+const SaveSoldiers = (soldiers) => {
+    localStorage.setItem("soldiers", JSON.stringify(soldiers));
+}
+
+const AddSoldier = () => {
+    const [fullName, rank, position, platoon, missionTime] = document.querySelectorAll("input")
+    const status = document.querySelector("#status")
+
+    const soldier = {
+        fullName: fullName.value,
+        missionTime: missionTime.value,
+        platoon: platoon.value,
+        position: position.value,
+        rank: rank.value,
+        status: status.value,
+    }
+
+    if (localStorage.getItem("soldiers")) {
+        const soldiers = GetAllSoldiers()
+        soldiers.push(soldier)
+        SaveSoldiers(soldiers)
+    }
+    else {
+        const soldiers = [soldier];
+        SaveSoldiers(soldiers)
+    }
+}
+
+const CreateSoldier = (soldier) => {
+    const row = document.createElement("tr")
+
+    const name = document.createElement("td")
+    const rank = document.createElement("td")
+    const position = document.createElement("td")
+    const platoon = document.createElement("td")
+    const status = document.createElement("td")
+    const actions = document.createElement("td")
+
+    name.textContent = soldier.fullName
+    rank.textContent = soldier.rank
+    position.textContent = soldier.position
+    platoon.textContent = soldier.platoon
+    status.textContent = soldier.status
+
+    const actionsDiv = document.createElement("div")
+    actionsDiv.className = "actions"
+
+    const remove = document.createElement("div")
+    remove.className = "action"
+    remove.textContent = "Remove"
+
+    
+    const edit = document.createElement("div")
+    edit.className = "action"
+    edit.textContent = "Edit"
+    
+    actionsDiv.appendChild(remove)
+    if (soldier.status !== "Retired"){
+        const mission = document.createElement("div")
+        mission.className = "action"
+        mission.textContent = "Mission"
+        actionsDiv.appendChild(mission)
+    }
+    actionsDiv.appendChild(edit)
+    actions.appendChild(actionsDiv)
+
+    row.appendChild(name)
+    row.appendChild(rank)
+    row.appendChild(position)
+    row.appendChild(platoon)
+    row.appendChild(status)
+    row.appendChild(actions)
+
+    return row
+}
+
+const PrintSoldiers = () => {
+    const table = document.querySelector("table")
+    table.innerHTML = ""
+    const title = document.createElement("tr")
+
+    const tit1 = document.createElement("th")
+    const tit2 = document.createElement("th")
+    const tit3 = document.createElement("th")
+    const tit4 = document.createElement("th")
+    const tit5 = document.createElement("th")
+    const tit6 = document.createElement("th")
+
+    const name = document.createElement("div")
+    name.className = "action"
+    name.textContent = "Full Name ↓"
+    tit1.appendChild(name)
+    tit2.textContent = "RANK"
+    tit3.textContent = "POSITION"
+    tit4.textContent = "PLATON"
+    tit5.textContent = "STATUS"
+    tit6.textContent = "ACTIONS"
+    
+    title.appendChild(tit1)
+    title.appendChild(tit2)
+    title.appendChild(tit3)
+    title.appendChild(tit4)
+    title.appendChild(tit5)
+    title.appendChild(tit6)
+    table.appendChild(title)
+
+    const soldiers = GetAllSoldiers()
+    for (const soldier of soldiers){
+        const row = CreateSoldier(soldier)
+        table.appendChild(row)
+    }
+}
+
+PrintSoldiers()
+
+const deleteSoldier = (soldier) => {
+    const soldiers = GetAllSoldiers()
+    const newSoldiers = 
+}
